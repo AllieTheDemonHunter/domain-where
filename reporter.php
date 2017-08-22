8e6f21460b94f2a1005c2b5f7dd9cf3d609d7189
Allie du Plooy <allie.rockdev@gmail.com>
2017-08-22 07:56:18 +0200

    Changes.

--- reporter.php
+++ reporter.php
@@ -7,14 +7,4 @@ include_once "reporter/reporterRemote.php";
 
 $reporter = new _reporterRemote($_SERVER['SERVER_NAME']);
 $response['remote'] = $reporter;
-
-//header('Content-Type: application/json; charset=utf-8;');
-//header("Transfer-Encoding: identity;");
 print json_encode($response);
-ob_start();
-try {
-  $response['update'] = `git checkout master && git pull`;
-} catch (Exception $e) {
-  print "Shell access is not allowed on the operating system.";
-}
-die();
