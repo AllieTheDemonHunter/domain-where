283b6c8d254579b2162c8b521da53e4b11eafd8a
Allie du Plooy <allie.rockdev@gmail.com>
2017-09-05 19:53:49 +0200

    More.

--- reporter.php
+++ reporter.php
@@ -5,8 +5,12 @@ include_once "reporter/convenience.php";
 include_once "reporter/reporter.php";
 include_once "reporter/reporterRemote.php";
 $limit_in_minutes = 2;
-print $then = `git log -1 --pretty=format:%ct`;
-print "---";
+$limit_in_seconds = $limit_in_minutes * 60;
+$then = `git log -1 --pretty=format:%ct`;
 $now = time();
-print $difference_in_seconds = "difference_in_seconds:".($then - $now);
+$difference_in_seconds = abs($then - $now);
+if($difference_in_seconds > $difference_in_seconds) {
+    print `git pull`;
+    die("updating");
+}
 print json_encode(new _reporterRemote($_SERVER['SERVER_NAME']));
