392cef9a85e25514150db0f5ed6790671a321107
Allie du Plooy <allie.rockdev@gmail.com>
2017-09-05 21:00:00 +0200

    More.

--- reporter.php
+++ reporter.php
@@ -9,9 +9,23 @@ $limit_in_seconds = $limit_in_minutes * 60;
 $then = `git log -1 --pretty=format:%ct`;
 $now = time();
 $difference_in_seconds = abs($then - $now);
-if($difference_in_seconds < $limit_in_seconds && file_exists("tmp.json")) {
+$updating = FALSE;
+$execute_report = FALSE;
+if ($difference_in_seconds < $limit_in_seconds) {
     print `git pull`;
-    die(file_get_contents("tmp.json"));
+    $updating = TRUE;
 }
-$result = json_encode(new _reporterRemote($_SERVER['SERVER_NAME']));
-file_put_contents("tmp.json", $result);
+if (file_exists("tmp.json") && $updating) {
+    //Updating with a cached result.
+    print file_get_contents("tmp.json");
+} elseif ($updating) {
+    //Updating with no cached result.
+    //Create a result:
+    //Last option is to return live results.
+    $result = json_encode(new _reporterRemote($_SERVER['SERVER_NAME']));
+    file_put_contents("tmp.json", $result);
+    print $result;
+} elseif (file_exists("tmp.json")) {
+    //Not updating, and has 'new enough' version cached.
+    print file_get_contents("tmp.json");
+}
