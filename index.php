7fa06b5c962d549bf798706d9962adc42e00aac3
Allie du Plooy <allie.rockdev@gmail.com>
2017-09-07 10:51:27 +0200

    Merging in the table_style branch.

--- index.php
+++ index.php
@@ -33,9 +33,9 @@ if($_GET['debug']) {
 <div id="wrapper">
   <?php
   $domains["http://z-starbright.co.za.dedi25.cpt4.host-h.net"] = 1;
-  $domains["http://z-dtm.co.za.dedi1255.jnb1.host-h.net"] = 1;
-  $domains["http://z-dspsa.co.za.dedi179.cpt3.host-h.net"] = 0;
-  $domains["https://www.ferreirapartners.co.za"] = 0;
+  $domains["http://z-dtm.co.za.dedi1255.jnb1.host-h.net"] = 0;
+  $domains["http://z-dspsa.co.za.dedi179.cpt3.host-h.net"] = 1;
+  $domains["https://www.ferreirapartners.co.za"] = 1;
   $domains["http://sph"] = 0;
 
   $requests = ["analytics", "loadaverage", "disk", "drush", "psi"];
